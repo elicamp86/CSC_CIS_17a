3eaf677bdba7762a8e1f354d66547a124f49f198
eliavelina <eliavelina@gmail.com>
2026-09-13 16:17:28 -0700

    Add IntroLab Hello World

---
+++ Lab/IntroLab/main.cpp
@@ -0,0 +1,17 @@
+/*
+*Author: Elizabeth Campuzano
+*Date: September 13, 2026
+*Purpose: Hello World
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv) 
+{
+    
+    cout << endl << "   Hello World!  " <<endl <<endl;
+
+  
+    return 0;
+}
